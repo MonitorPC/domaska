e75a73dcb768c5d480f8a53f58b09ecb8ff043c7
MonitorPC <48449929+MonitorPC@users.noreply.github.com>
2020-05-25 19:09:04 +0300

    Update ctenography.py

--- ctenography.py
+++ ctenography.py
@@ -2,12 +2,12 @@ from PIL import Image, ImageDraw
 from random import randint
 import os
 
-a = input('1 - закодировать, 2 - декодировать: \n')
-if a == '1':
-    if not os.path.exists('codirovka'):
-        os.mkdir('codirovka')
 
-    keys = open('codirovka/key.txt', 'w')
+def encode():
+    if not os.path.exists('coding_img'):
+        os.mkdir('coding_img')
+
+    keys = open('coding_img/key.txt', 'w')
 
     img = Image.open(input('Путь до картинки: '))
     draw = ImageDraw.Draw(img)
@@ -24,32 +24,29 @@ if a == '1':
             list_elem.append([ord(i) - 1000, 'r'])
 
     for elem in list_elem:
-        print(elem)
         key = (randint(1, width - 1), randint(1, height - 1))
         red, green = pixels[key][:2]
         draw.point(key, (red, green, elem[0]))
+
         if elem[-1] == 'r':
-            keys.write(str(key[0]) + ' ' + str(key[-1]) + ' ' + 'r' + ' ' + '\n')
+            keys.write(str(key[0]) + ' ' + str(key[-1]) + ' ' + 'r' + '\n')
         else:
             keys.write(str(key[0]) + ' ' + str(key[-1]) + '\n')
 
-    img.save('codirovka/zacodirovan.png', 'PNG')
+    img.save('coding_img/coded_img.png', 'PNG')
     keys.close()
 
-elif a == '2':
 
-    path_file = input('Путь до папки "codirovka": ')
-
-    keys = open(f'{path_file}\key.txt')
+def decode():
+    keys = open(input('Путь до "key.txt": '))
     keys = [i.strip().split(' ') for i in keys.readlines()]
 
-    img = Image.open(f'{path_file}\zacodirovan.png')
+    img = Image.open(input('Путь до картинки с сообщением: '))
 
     pixels = img.load()
     message = ''
 
     for i in keys:
-        print(i)
         if i[-1] == 'r':
             print(pixels[int(i[0]), int(i[1])][2] + 1000)
             message += chr(pixels[int(i[0]), int(i[1])][2] + 1000)
@@ -57,3 +54,13 @@ elif a == '2':
             message += chr(pixels[int(i[0]), int(i[1])][2])
 
     print('Ваше сообщение: ' + message)
+
+
+while True:
+    a = input('1 - закодировать, 2 - декодировать: ')
+    if a == '1':
+        encode()
+    elif a == '2':
+        decode()
+    else:
+        break
